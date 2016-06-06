c2aa84d07307fa1ae80ed50108188a62567b3853
Víctor A. Rodríguez (Bit-Man) <victor@bit-man.guru>
2016-06-05 21:37:17 -0300

    Multiple requests

--- src/main/java/bitman/Server.java
+++ src/main/java/bitman/Server.java
@@ -8,7 +8,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) throws IOException {
+    private final int port;
+
+    public static void main(String[] args) throws IOException {
 
 		if (args.length != 1) {
 			System.err.println("Usage: java Server <port number>");
@@ -17,23 +19,76 @@ public class Server {
 
 		int portNumber = Integer.parseInt(args[0]);
 
-		try (
-				ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-				Socket clientSocket = serverSocket.accept();
-				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-		) {
-
-			String inputLine;
-			// TODO Let the server continue running once the command is executed
-			while ((inputLine = in.readLine()) != null) {
-				Command cmd = CommandFactory.createCommand(inputLine);
-				cmd.execute(out);
-			}
-
-		} catch (IOException e) {
-			System.err.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
-			System.err.println(e.getMessage());
-		}
+        new Server(portNumber).mainLoop();
 	}
+
+    private Server(int port)
+    {
+        this.port = port;
+    }
+
+    private void mainLoop()
+    {
+        try (
+                ServerSocket serverSocket = new ServerSocket(port)
+        )
+        {
+            while (true)
+            {
+                Socket clientSocket = getConnection(serverSocket);
+                new Thread(new ClientRequest(clientSocket)).start();
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private Socket getConnection(ServerSocket serverSocket)
+    {
+        while (true)
+        {
+            try
+            {
+                return serverSocket.accept();
+            } catch (IOException e)
+            {
+                e.printStackTrace(System.err);
+            }
+        }
+    }
+
+    private class ClientRequest
+            implements Runnable
+    {
+        private Socket clientSocket;
+
+        public ClientRequest(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+        }
+
+
+        @Override
+        public void run()
+        {
+            try (
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            )
+            {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                {
+                    Command cmd = CommandFactory.createCommand(inputLine);
+                    cmd.execute(out);
+                }
+
+            } catch (IOException e)
+            {
+                e.printStackTrace(System.err);
+            }
+
+        }
+    }
 }
